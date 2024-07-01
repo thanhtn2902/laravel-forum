e4929ae9f0c968753290119f17aaeb51264124ad
Thanh Truong Nguyen <thanh.truong1@motorolasolutions.com>
2024-07-01 10:06:33 +0700

    add api resource

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'body'  => $this->body,
+            'user' => UserResource::make($this->user),
+            'post' => PostResource::make($this->post),
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::parse($this->updated_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name'  => $this->name,
+            'email' => $this->when($this->id === $request->user?->id, $this->email),
+            'created_at'    => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
+            'updated_at'    => Carbon::parse($this->updated_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
