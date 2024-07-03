d793786877b3f83eb1c9dde46e36999a317b2075
Thanh Truong Nguyen <thanh.truong1@motorolasolutions.com>
2024-07-03 09:48:47 +0700

    update UI for comment and update timezone

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -14,6 +14,22 @@
             <div class="mt-12">
                 <h2 class="text-2xl font-semibold">Comments</h2>
 
+                <form v-if="$page.props.auth.user" @submit.prevent="addComment">
+                    <div>
+                        <InputLabel for="body" class="sr-only">Comment</InputLabel>
+                        <TextArea id="body" v-model="commentForm.body" placeholder="Your comment here!" rows="4" />
+                        <InputError :message="commentForm.errors.body" class="mt-2"/>
+                    </div>
+
+                    <PrimaryButton
+                        type="submit"
+                        class="mt-3 disabled:opacity-40 disabled:cursor-not-allowed"
+                        :disabled="commentForm.processing"
+                    >
+                        Add Comment
+                    </PrimaryButton>
+                </form>
+
                 <ul class="divide-y mt-4">
                     <li v-for="comment in comments.data" :key="comment.id" class="px-2 py-4">
                         <Comment :comment="comment"/>
@@ -28,14 +44,28 @@
 
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import { computed } from 'vue';
 import Container from '@/Components/Container.vue';
 import Pagination from '@/Components/Pagination.vue';
-import { relativeDate } from '@/Utilities/Date.js'
 import Comment from '@/Components/Comment.vue'
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextArea from '@/Components/TextArea.vue';
+import { computed } from 'vue';
+import { useForm } from '@inertiajs/vue3';
+import { relativeDate } from '@/Utilities/Date.js'
+import InputError from '@/Components/InputError.vue';
 
 const props = defineProps(['post', 'comments'])
 
 const postedDate = computed(() => relativeDate(props.post.created_at))
+
+const commentForm = useForm({
+    body: '',
+})
+
+const addComment = () => commentForm.post(route('posts.comments.store', props.post.id), {
+    preserveScroll: true,
+    onSuccess: () => commentForm.reset()
+})
 
 </script>
