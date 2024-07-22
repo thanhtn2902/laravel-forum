a818c5b6117a51d305bf5c02c008a99c78aa29bb
Thanh Truong Nguyen <thanh.truong1@motorolasolutions.com>
2024-07-22 15:12:23 +0700

    implement search with topic

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -6,16 +6,27 @@
                 <p v-if="selectedTopic" class="mt-1 text-gray-600 text-sm">{{ selectedTopic.description }}</p>
                 <menu class="flex space-x-2 mt-4 pb-3 pt-1 overflow-x-auto">
                     <li>
-                        <Pill :href="route('posts.index')" :filled="!selectedTopic">
+                        <Pill :href="route('posts.index', { query: searchForm.query })" :filled="!selectedTopic">
                             All post
                         </Pill>
                     </li>
                     <li v-for="topic in topics" :key="topic.id" >
-                        <Pill :href="route('posts.index', {'topic': topic.slug})" :filled="selectedTopic?.id === topic.id">
+                        <Pill :href="route('posts.index', {'topic': topic.slug, query: searchForm.query })" :filled="selectedTopic?.id === topic.id">
                             {{ topic.name }}
                         </Pill>
                     </li>
                 </menu>
+
+                <form @submit.prevent="search" class="mt-4">
+                    <div>
+                        <InputLabel for="query">Search</InputLabel>
+                        <div class="flex space-x-2 mt-1 ">
+                            <TextInput class="w-full" id="query" v-model="searchForm.query" />
+                            <SecondaryButton type="submit">Search</SecondaryButton>
+                            <DangerButton v-if="searchForm.query" @click="clearSearch">Clear</DangerButton>
+                        </div>
+                    </div>
+                </form>
             </div>
             <ul class="divide-y space-y-2 mt-4">
                 <li v-for="post in posts.data" :key="post.id">
@@ -37,13 +48,29 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Container from '@/Components/Container.vue';
 import Pagination from '@/Components/Pagination.vue';
-import { Link } from '@inertiajs/vue3';
+import { Link, useForm, usePage } from '@inertiajs/vue3';
 import { relativeDate } from '@/Utilities/Date.js';
 import PageHeading from '@/Components/PageHeading.vue';
 import Pill from '@/Components/Pill.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import DangerButton from '@/Components/DangerButton.vue';
 
-defineProps(['posts', 'selectedTopic', 'topics']);
+const props = defineProps(['posts', 'selectedTopic', 'topics', 'query']);
 
 const formattedDate = (post) => relativeDate(post.created_at)
+
+const searchForm = useForm({
+    query: props.query,
+    page: 1
+})
+
+const page = usePage()
+const search = () => searchForm.get(page.url)
+const clearSearch = () => {
+    searchForm.query = ''
+    search()
+}
 
 </script>
